8d0676f14ac0bd66bb6813c22f82ee38bb82e7e7
s30919 <s30919@pjwstk.edu.pl>
2025-05-11 21:42:33 +0300

    DTO

--- Tutorial9/Services/IDbService.cs
+++ Tutorial9/Services/IDbService.cs
@@ -1,7 +1,12 @@
 ﻿namespace Tutorial9.Services;
+using Tutorial9.Model.DTOs;
 
 public interface IDbService
 {
     Task DoSomethingAsync();
     Task ProcedureAsync();
+    Task<int> AddProductToWarehouseAsync(WarehouseRequest request);
+    Task<int> AddProductWithProcedureAsync(WarehouseRequest request);
+
+
 }
